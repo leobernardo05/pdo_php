5f02c574187a29cef6e8fd4f946df70952d012d1
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-06-16 15:54:59 -0300

    update

--- index.php
+++ index.php
@@ -7,46 +7,12 @@ $senha = '';
 try {
 
     $conexao = new PDO($dsn, $usuario, $senha);
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Criação da tabela
-    $query = "
-        CREATE TABLE IF NOT EXISTS tb_usuarios (
-            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            nome VARCHAR(50) NOT NULL,
-            email VARCHAR(100) NOT NULL,
-            senha VARCHAR(32) NOT NULL
-        )
-    ";
+    $query = '
+        select * from tb_usuarios
+    ';
 
-    $conexao->exec($query);
-
-    // Inserções
-    $query = "
-        INSERT INTO tb_usuarios (nome, email, senha)
-        VALUES ('Leonardo', 'leodesenvolvedor@gmail.com', '123456')
-    ";
-    echo $conexao->exec($query) . "<br>";
-
-    $query = "
-        INSERT INTO tb_usuarios (nome, email, senha)
-        VALUES ('Jose Rivaldo', 'josedesenvolvedor@gmail.com', '654321')
-    ";
-    echo $conexao->exec($query) . "<br>";
-
-    $query = "
-        INSERT INTO tb_usuarios (nome, email, senha)
-        VALUES ('Romario', 'romariode@gmail.com', '654321')
-    ";
-    echo $conexao->exec($query) . "<br>";
-
-    $query = "
-        INSERT INTO tb_usuarios (nome, email, senha)
-        VALUES ('Jose Romildo', 'romildodesenvolvedor@gmail.com', '654321')
-    ";
-    echo $conexao->exec($query) . "<br>";
-
-    echo "Registros inseridos com sucesso!";
+    $conexao->query($query);
 
 } catch (PDOException $e) {
 
