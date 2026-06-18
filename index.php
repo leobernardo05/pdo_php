9fb77dd4616ef3e93b67e88f0b4c098fc9781897
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-06-18 10:35:43 -0300

    update

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
-    if(!empty($_POST['usuario']) && !empty($_POST['senha']))
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
+        $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+        $usuario = 'root';
+        $senha = '';
 
-    try {
-        $conexao = new PDO($dsn, $usuario, $senha);
-        $query .= "email = ''"
-    } catch (PDOException $e) {
-        echo 'Erro: ' . $e->getCode() . ' - Mensagem: ' . $e->getMessage();
+        try {
+            $conexao = new PDO($dsn, $usuario, $senha);
+
+            // query
+            $query = "select * from tb_usuarios where";
+            $query .= "email = '{$_POST['usuario']}'";
+            $query .= " AND senha = '{$_POST['senha']}'";
+            echo $query;
+
+            $stmt = $conexao->query($query);
+            $usuario = $stmt->fetch();
+
+        } catch (PDOException $e) {
+            echo 'Erro: ' . $e->getCode() . ' - Mensagem: ' . $e->getMessage();
+        }
     }
 ?>
 
